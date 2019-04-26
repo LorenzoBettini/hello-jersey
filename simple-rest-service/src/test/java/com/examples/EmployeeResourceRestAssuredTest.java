88edcd1deef7f14aa7ed78f45261759519d6c07a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-04-26 16:42:17 +0200

    Employee first end-point

---
+++ simple-rest-service/src/test/java/com/examples/EmployeeResourceRestAssuredTest.java
@@ -0,0 +1,51 @@
+package com.examples;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class EmployeeResourceRestAssuredTest {
+
+	private HttpServer server;
+
+	@Before
+	public void setUp() throws Exception {
+		server = Main.startServer();
+	}
+
+	@BeforeClass
+	public static void configureRestAssured() {
+		RestAssured.baseURI = Main.BASE_URI;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		server.shutdownNow();
+	}
+
+	@Test
+	public void testGetItXML() {
+		given().
+			accept(MediaType.APPLICATION_XML).
+		when().
+			get("employee").
+		then().
+			statusCode(200).
+			assertThat().
+				body(
+					"employee.id", equalTo("E1"),
+					"employee.name", equalTo("An employee"),
+					"employee.salary", equalTo("1000")
+				);
+	}
+
+}
