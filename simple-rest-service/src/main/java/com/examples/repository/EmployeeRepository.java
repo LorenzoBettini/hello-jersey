fb7d448137f9b7c4d1668e60faef74aff2befed3
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-04-28 18:12:16 +0200

    EmployeeRepository interface

--- simple-rest-service/src/main/java/com/examples/repository/EmployeeRepository.java
+++ simple-rest-service/src/main/java/com/examples/repository/EmployeeRepository.java
@@ -1,38 +1,14 @@
 package com.examples.repository;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import com.examples.model.Employee;
 
-/**
- * An example repository for employees.
- * 
- * In a real application this should be handled by a database.
- */
-public class EmployeeRepository {
+public interface EmployeeRepository {
 
-	public static final EmployeeRepository instance =
-		new EmployeeRepository();
+	List<Employee> findAll();
 
-	private List<Employee> employees = new LinkedList<>();
+	Optional<Employee> findOne(String id);
 
-	private EmployeeRepository() {
-		// initialize the "db" with some contents
-		employees.add(new Employee("ID1", "First Employee", 1000));
-		employees.add(new Employee("ID2", "Second Employee", 2000));
-		employees.add(new Employee("ID3", "Third Employee", 3000));
-	}
-
-	public List<Employee> findAll() {
-		return employees;
-	}
-
-	public Optional<Employee> findOne(String id) {
-		return employees.
-				stream().
-				filter(e -> e.getEmployeeId().equals(id)).
-				findFirst();
-	}
 }
