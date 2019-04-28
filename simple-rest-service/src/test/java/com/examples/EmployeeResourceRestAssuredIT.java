11a8a12f92b3b373764cd4cde8d469993800c402
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-04-28 20:56:04 +0200

    use JerseyTest and Mockito

--- simple-rest-service/src/test/java/com/examples/EmployeeResourceRestAssuredIT.java
+++ simple-rest-service/src/test/java/com/examples/EmployeeResourceRestAssuredIT.java
@@ -1,70 +1,40 @@
 package com.examples;
 
 import static io.restassured.RestAssured.*;
-import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
-import com.examples.model.Employee;
-import com.examples.repository.EmployeeRepository;
 
 import io.restassured.RestAssured;
 
-public class EmployeeResourceRestAssuredTest extends JerseyTest {
+public class EmployeeResourceRestAssuredIT {
 
 	private static final String EMPLOYEES = "employees";
+	private HttpServer server;
 
-	@Mock
-	private EmployeeRepository employeeRepository;
-
-	@Override
-	protected Application configure() {
-		MockitoAnnotations.initMocks(this);
-		// register only the EmployeeResource
-		return new ResourceConfig(EmployeeResource.class)
-			// we must make Jersey aware of our mapper
-			// since we don't make it scan the whole package
-			.register(NotFoundMapper.class)
-			// inject the mock in our EmployeeResource
-			.register(new AbstractBinder() {
-				@Override
-				protected void configure() {
-					// differently from Guice,
-					// bind(concrete).to(abstract)
-					bind(employeeRepository)
-						.to(EmployeeRepository.class);
-				}
-			});
+	@BeforeClass
+	public static void configureRestAssured() {
+		RestAssured.baseURI = Main.BASE_URI;
 	}
 
 	@Before
-	public void configureRestAssured() {
-		// retrieve the base URI of the JerseyTest server
-		RestAssured.baseURI = getBaseUri().toString();
+	public void setUp() throws Exception {
+		server = Main.startServer();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		server.shutdownNow();
 	}
 
 	@Test
 	public void testGetAllEmployees() {
-		when(employeeRepository.findAll())
-			.thenReturn(asList(
-				new Employee("ID1", "First Employee", 1000),
-				new Employee("ID2", "Second Employee", 2000)
-			));
-
 		given().
 			accept(MediaType.APPLICATION_XML).
 		when().
@@ -78,36 +48,55 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 			"employees.employee[0].salary", equalTo("1000"),
 			"employees.employee[1].id", equalTo("ID2"),
 			"employees.employee[1].name", equalTo("Second Employee"),
-			"employees.employee[1].salary", equalTo("2000")
+			"employees.employee[1].salary", equalTo("2000"),
+			"employees.employee[2].id", equalTo("ID3"),
+			"employees.employee[2].name", equalTo("Third Employee"),
+			"employees.employee[2].salary", equalTo("3000")
 			);
 	}
 
-
 	@Test
-	public void testGetOneEmployee() {
-		when(employeeRepository.findOne("ID1"))
-			.thenReturn(
-				Optional.of(new Employee("ID1", "An Employee", 2000)));
-
+	public void testGetAllEmployeesWithRootPaths() {
+		// a variation of the above test showing how to
+		// test several XML elements
 		given().
 			accept(MediaType.APPLICATION_XML).
 		when().
-			get(EMPLOYEES + "/ID1").
+			get(EMPLOYEES).
+		then().
+			statusCode(200).
+			assertThat().
+				root("employees.employee[0]").
+				body(
+					"id", equalTo("ID1"),
+					"name", equalTo("First Employee"),
+					"salary", equalTo("1000")
+				).
+				root("employees.employee[1]").
+				body(
+					"id", equalTo("ID2")
+					// similar assertions for the other fields
+				);
+	}
+
+	@Test
+	public void testGetOneEmployee() {
+		given().
+			accept(MediaType.APPLICATION_XML).
+		when().
+			get(EMPLOYEES + "/ID2").
 		then().
 			statusCode(200).
 			assertThat().
 			body(
-				"employee.id", equalTo("ID1"),
-				"employee.name", equalTo("An Employee"),
+				"employee.id", equalTo("ID2"),
+				"employee.name", equalTo("Second Employee"),
 				"employee.salary", equalTo("2000")
 			);
 	}
 
 	@Test
 	public void testGetOneEmployeeWithNonExistingId() {
-		when(employeeRepository.findOne(Mockito.anyString()))
-			.thenReturn(Optional.empty());
-
 		given().
 			accept(MediaType.APPLICATION_XML).
 		when().
@@ -120,12 +109,6 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetAllEmployeesJSON() {
-		when(employeeRepository.findAll())
-			.thenReturn(asList(
-				new Employee("ID1", "First Employee", 1000),
-				new Employee("ID2", "Second Employee", 2000)
-			));
-
 		given().
 			accept(MediaType.APPLICATION_JSON).
 		when().
@@ -147,20 +130,16 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetOneEmployeeJSON() {
-		when(employeeRepository.findOne("ID1"))
-			.thenReturn(
-				Optional.of(new Employee("ID1", "An Employee", 2000)));
-
 		given().
 			accept(MediaType.APPLICATION_JSON).
 		when().
-			get(EMPLOYEES + "/ID1").
+			get(EMPLOYEES + "/ID2").
 		then().
 			statusCode(200).
 			assertThat().
 			body(
-				"id", equalTo("ID1"),
-				"name", equalTo("An Employee"),
+				"id", equalTo("ID2"),
+				"name", equalTo("Second Employee"),
 				"salary", equalTo(2000)
 				// NOTE: "salary" retains its integer type in JSON
 				// so it must be equal to 2000 NOT "2000"
@@ -169,9 +148,6 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetOneEmployeeWithNonExistingIdJSON() {
-		when(employeeRepository.findOne(Mockito.anyString()))
-			.thenReturn(Optional.empty());
-
 		given().
 			accept(MediaType.APPLICATION_JSON).
 		when().
