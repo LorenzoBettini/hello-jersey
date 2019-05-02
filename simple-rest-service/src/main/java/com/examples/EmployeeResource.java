31c2ec2be60c9fb222cda1ff9c5784bef755ae11
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-02 11:40:02 +0200

    implemented POST

--- simple-rest-service/src/main/java/com/examples/EmployeeResource.java
+++ simple-rest-service/src/main/java/com/examples/EmployeeResource.java
@@ -3,12 +3,16 @@ package com.examples;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.examples.model.Employee;
 import com.examples.repository.EmployeeRepository;
@@ -50,5 +54,22 @@ public class EmployeeResource {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getCount() {
 		return String.valueOf(employeeRepository.findAll().size());
+	}
+
+	/**
+	 * Add a new Employee to the database. For simplicity, we assume that the
+	 * operation always succeeds.
+	 * 
+	 * @param employee
+	 * @return
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addEmployee(Employee employee) {
+		return Response
+			.status(Status.CREATED)
+			.entity(employeeRepository.save(employee))
+			.build();
 	}
 }
