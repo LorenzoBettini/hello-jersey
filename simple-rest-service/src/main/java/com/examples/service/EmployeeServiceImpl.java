faa01a7f907cc88b1180214a9557d7a00c36574e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-04 16:35:55 +0200

    implemented EmployeeService

---
+++ simple-rest-service/src/main/java/com/examples/service/EmployeeServiceImpl.java
@@ -0,0 +1,58 @@
+package com.examples.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+
+import com.examples.model.Employee;
+import com.examples.repository.EmployeeRepository;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+
+	@Inject
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public List<Employee> allEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee getEmployeeById(String id) {
+		return employeeRepository
+			.findOne(id)
+			.orElseThrow(
+				() -> new NotFoundException("Employee not found with id " + id));
+	}
+
+	@Override
+	public Employee addEmployee(Employee employee) {
+		sanityChecks(employee);
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee replaceEmployeeById(String id, Employee employee) {
+		sanityChecks(employee);
+		if (!employeeRepository.findOne(id).isPresent())
+			throw new NotFoundException("Employee not found with id " + id);
+		employee.setEmployeeId(id);
+		return employeeRepository.save(employee);
+	}
+
+	private void sanityChecks(Employee employee) {
+		if (employee == null) {
+			throw new BadRequestException("Missing values for Employee");
+		}
+		if (employee.getEmployeeId() != null) {
+			throw new BadRequestException("Unexpected id specification for Employee");
+		}
+	}
+
+}
