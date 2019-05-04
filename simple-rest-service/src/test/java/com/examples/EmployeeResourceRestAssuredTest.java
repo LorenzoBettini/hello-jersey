07b5c0585bfef5f63c4edc5e22cf120c57335b61
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-04 18:28:33 +0200

    updated "unit" tests

--- simple-rest-service/src/test/java/com/examples/EmployeeResourceRestAssuredTest.java
+++ simple-rest-service/src/test/java/com/examples/EmployeeResourceRestAssuredTest.java
@@ -1,12 +1,14 @@
 package com.examples;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -19,11 +21,10 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.examples.model.Employee;
-import com.examples.repository.EmployeeRepository;
+import com.examples.service.EmployeeService;
 
 import io.restassured.RestAssured;
 
@@ -32,24 +33,21 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 	private static final String EMPLOYEES = "employees";
 
 	@Mock
-	private EmployeeRepository employeeRepository;
+	private EmployeeService employeeService;
 
 	@Override
 	protected Application configure() {
 		MockitoAnnotations.initMocks(this);
 		// register only the EmployeeResource
 		return new ResourceConfig(EmployeeResource.class)
-			// we must make Jersey aware of our mapper
-			// since we don't make it scan the whole package
-			.register(NotFoundMapper.class)
 			// inject the mock in our EmployeeResource
 			.register(new AbstractBinder() {
 				@Override
 				protected void configure() {
 					// differently from Guice,
 					// bind(concrete).to(abstract)
-					bind(employeeRepository)
-						.to(EmployeeRepository.class);
+					bind(employeeService)
+						.to(EmployeeService.class);
 				}
 			});
 	}
@@ -72,7 +70,7 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetAllEmployees() {
-		when(employeeRepository.findAll())
+		when(employeeService.allEmployees())
 			.thenReturn(asList(
 				new Employee("ID1", "First Employee", 1000),
 				new Employee("ID2", "Second Employee", 2000)
@@ -98,9 +96,9 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetOneEmployee() {
-		when(employeeRepository.findOne("ID1"))
+		when(employeeService.getEmployeeById(anyString()))
 			.thenReturn(
-				Optional.of(new Employee("ID1", "An Employee", 2000)));
+				new Employee("ID1", "An Employee", 2000));
 
 		given().
 			accept(MediaType.APPLICATION_XML).
@@ -117,23 +115,8 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 	}
 
 	@Test
-	public void testGetOneEmployeeWithNonExistingId() {
-		when(employeeRepository.findOne(Mockito.anyString()))
-			.thenReturn(Optional.empty());
-
-		given().
-			accept(MediaType.APPLICATION_XML).
-		when().
-			get(EMPLOYEES + "/foo").
-		then().
-			statusCode(404). // Status code: Not Found
-			contentType(MediaType.TEXT_PLAIN).
-			body(equalTo("Employee not found with id foo"));
-	}
-
-	@Test
 	public void testGetAllEmployeesJSON() {
-		when(employeeRepository.findAll())
+		when(employeeService.allEmployees())
 			.thenReturn(asList(
 				new Employee("ID1", "First Employee", 1000),
 				new Employee("ID2", "Second Employee", 2000)
@@ -160,9 +143,9 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 
 	@Test
 	public void testGetOneEmployeeJSON() {
-		when(employeeRepository.findOne("ID1"))
+		when(employeeService.getEmployeeById(anyString()))
 			.thenReturn(
-				Optional.of(new Employee("ID1", "An Employee", 2000)));
+				new Employee("ID1", "An Employee", 2000));
 
 		given().
 			accept(MediaType.APPLICATION_JSON).
@@ -181,24 +164,9 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 	}
 
 	@Test
-	public void testGetOneEmployeeWithNonExistingIdJSON() {
-		when(employeeRepository.findOne(Mockito.anyString()))
-			.thenReturn(Optional.empty());
-
-		given().
-			accept(MediaType.APPLICATION_JSON).
-		when().
-			get(EMPLOYEES + "/foo").
-		then().
-			statusCode(404). // Status code: Not Found
-			contentType(MediaType.TEXT_PLAIN).
-			body(equalTo("Employee not found with id foo"));
-	}
-
-	@Test
 	public void testCount() {
 		List<Employee> employees = asList(new Employee(), new Employee());
-		when(employeeRepository.findAll())
+		when(employeeService.allEmployees())
 			.thenReturn(employees);
 
 		when().
@@ -218,9 +186,9 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 				.build();
 
 		// when we pass an Employee with the values of the request body
-		when(employeeRepository.save(new Employee(null, "passed name", 1000)))
+		when(employeeService.addEmployee(new Employee(null, "passed name", 1000)))
 			.thenReturn(new Employee("ID", "returned name", 2000));
-			// the repository returns a new Employee object
+			// the service returns a new Employee object
 			// possibly with different values
 			// but for sure with a generated id
 
@@ -232,7 +200,7 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 		then().
 			statusCode(201).
 			assertThat().
-			// make sure we return the Employee returned by the repository
+			// make sure we return the Employee returned by the service
 			body(
 				"id", equalTo("ID"),
 				"name", equalTo("returned name"),
@@ -252,10 +220,10 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 				.build();
 
 		// when we pass an Employee with the values of the request body
-		// where we must have set the employee id of the URI
-		when(employeeRepository.save(new Employee("ID", "passed name", 1000)))
+		// where we must pass the employee id of the URI
+		when(employeeService.replaceEmployeeById("ID", new Employee(null, "passed name", 1000)))
 			.thenReturn(new Employee("ID", "returned name", 2000));
-			// the repository returns a new Employee object
+			// the service returns a new Employee object
 			// possibly with different values
 
 		given().
@@ -266,7 +234,7 @@ public class EmployeeResourceRestAssuredTest extends JerseyTest {
 		then().
 			statusCode(200).
 			assertThat().
-			// make sure we return the Employee returned by the repository
+			// make sure we return the Employee returned by the service
 			body(
 				"id", equalTo("ID"),
 				"name", equalTo("returned name"),
